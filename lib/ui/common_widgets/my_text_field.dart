59dcd4ba7ba034e7ea07cbabd68451d9a51b0cf7
Ahmad Khan <rmahmadkhan@icloud.com>
2022-04-16 15:55:31 +0500

    Created MyTextField

---
+++ lib/ui/common_widgets/my_text_field.dart
@@ -0,0 +1,74 @@
+import 'package:maveshi/all_utils.dart';
+
+class MyTextField extends StatelessWidget {
+  final TextInputType keyboardType;
+  final TextEditingController controller;
+  final bool obscure;
+  final String? hintText;
+  final bool enabled;
+  final bool readOnly;
+  final Widget? prefixWidget;
+  final Widget? suffixWidget;
+  final int maxLines;
+
+  const MyTextField({
+    required this.controller,
+    this.hintText,
+    this.keyboardType = TextInputType.text,
+    this.obscure = false,
+    this.enabled = true,
+    this.readOnly = false,
+    this.prefixWidget,
+    this.suffixWidget,
+    this.maxLines = 1,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: obscure,
+      controller: controller,
+      maxLines: maxLines,
+      enabled: enabled,
+      readOnly: readOnly,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please Enter a Valid Text';
+        }
+        return null;
+      },
+      keyboardType: keyboardType,
+      decoration: InputDecoration(
+        prefixIcon: prefixWidget == null
+            ? null
+            : Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: prefixWidget,
+              ),
+        suffixIcon: suffixWidget == null
+            ? null
+            : Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: suffixWidget,
+              ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(color: AppTheme.orangeColor),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(color: AppTheme.navyBlueColor),
+        ),
+        hintText: hintText,
+        fillColor: AppTheme.navyBlueColor,
+        hintStyle: const TextStyle(
+          fontSize: 14,
+          color: AppTheme.lightNavyBlueColor,
+          fontWeight: FontWeight.w600,
+        ),
+        // isDense: true,
+      ),
+    );
+  }
+}
