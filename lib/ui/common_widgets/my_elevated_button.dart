82bb8997f61b1c5fc06d642d31fb70f97cb16a31
Ahmad Khan <rmahmadkhan@icloud.com>
2022-04-16 15:55:18 +0500

    Created MyElevatedButton

---
+++ lib/ui/common_widgets/my_elevated_button.dart
@@ -0,0 +1,50 @@
+import 'package:maveshi/all_utils.dart';
+import 'package:maveshi/ui/common_widgets/my_text.dart';
+
+class MyElevatedButton extends StatelessWidget {
+  final Function(BuildContext context) onTap;
+  final String title;
+  final Color color;
+  final double width;
+  final double height;
+  final Color titleColor;
+  final EdgeInsetsGeometry margin;
+
+  const MyElevatedButton(
+    this.title, {
+    required this.onTap,
+    this.titleColor = AppTheme.whiteColor,
+    this.width = double.infinity,
+    this.height = 48,
+    this.color = AppTheme.orangeColor,
+    this.margin = EdgeInsets.zero,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: margin,
+      child: SizedBox(
+        width: width,
+        height: height,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              primary: color,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10))),
+          child: FittedBox(
+            fit: BoxFit.scaleDown,
+            child: MyText(
+              title,
+              color: titleColor,
+              fontSize: height / 2.5,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          onPressed: () => onTap(context),
+        ),
+      ),
+    );
+  }
+}
