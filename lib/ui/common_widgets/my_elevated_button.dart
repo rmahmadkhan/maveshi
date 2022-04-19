a8fdab33d708ce19c86f9ad1850b863e849a7c5c
Ahmad Khan <rmahmadkhan@icloud.com>
2022-04-19 16:25:07 +0500

    Created SocialLoginSection

--- lib/ui/common_widgets/my_elevated_button.dart
+++ lib/ui/common_widgets/my_elevated_button.dart
@@ -1,5 +1,4 @@
 import 'package:maveshi/all_utils.dart';
-import 'package:maveshi/ui/common_widgets/my_text.dart';
 
 class MyElevatedButton extends StatelessWidget {
   final Function(BuildContext context) onTap;
