0548c4fc6eb8d2bbabdae0e3386fa7631a0ec20a
Ahmad Khan <rmahmadkhan@icloud.com>
2022-04-25 00:29:51 +0500

    Added SingleChildScrollView over login screen widget tree

--- lib/ui/screens/login/login_screen.dart
+++ lib/ui/screens/login/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:maveshi/all_utils.dart';
 import 'package:maveshi/ui/screens/login/components/login_form.dart';
 import 'package:maveshi/ui/screens/login/components/social_login_section.dart';
+import 'package:maveshi/ui/screens/register/register_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   static const routeName = '/LoginScreen';
@@ -10,33 +11,35 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BaseScaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            const Spacer(),
-            const MyText('Welcome', fontSize: 30, fontWeight: FontWeight.bold),
-            const VerticalSpacing(of: 10),
-            const MyText(
-              'Please login to continue',
-              fontSize: 20,
-              color: AppTheme.greyColor,
-            ),
-            const VerticalSpacing(of: 50),
-            const LoginForm(),
-            const VerticalSpacing(of: 10),
-            Align(
-              alignment: Alignment.center,
-              child: MyTextButton(
-                onTap: (_) {},
-                prefixText: 'New to the app?',
-                title: 'Register',
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+          child: Column(
+            children: [
+              const MyText('Welcome',
+                  fontSize: 30, fontWeight: FontWeight.bold),
+              const VerticalSpacing(of: 10),
+              const MyText(
+                'Please login to continue',
+                fontSize: 20,
+                color: AppTheme.greyColor,
               ),
-            ),
-            const VerticalSpacing(),
-            const SocialLoginSection(),
-            const Spacer(),
-          ],
+              const VerticalSpacing(of: 50),
+              const LoginForm(),
+              const VerticalSpacing(of: 10),
+              Align(
+                alignment: Alignment.center,
+                child: MyTextButton(
+                  onTap: (_) =>
+                      Navigator.pushNamed(context, RegisterScreen.routeName),
+                  prefixText: 'New to the app?',
+                  title: 'Register',
+                ),
+              ),
+              const VerticalSpacing(),
+              const SocialLoginSection(),
+            ],
+          ),
         ),
       ),
     );
