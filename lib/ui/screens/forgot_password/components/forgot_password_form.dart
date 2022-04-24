7afd049289b48c9262016e863c86fe61c1b746d2
Ahmad Khan <rmahmadkhan@icloud.com>
2022-04-25 00:39:13 +0500

    Created ForgotPasswordForm

---
+++ lib/ui/screens/forgot_password/components/forgot_password_form.dart
@@ -0,0 +1,33 @@
+import 'package:maveshi/all_utils.dart';
+
+class ForgotPasswordForm extends StatefulWidget {
+  const ForgotPasswordForm({Key? key}) : super(key: key);
+
+  @override
+  State<ForgotPasswordForm> createState() => _ForgotPasswordFormState();
+}
+
+class _ForgotPasswordFormState extends State<ForgotPasswordForm> {
+  final emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        MyTextField(
+          controller: emailController,
+          hintText: 'Email',
+          keyboardType: TextInputType.emailAddress,
+        ),
+        const VerticalSpacing(),
+        MyElevatedButton('Next', onTap: (_) {}),
+      ],
+    );
+  }
+}
