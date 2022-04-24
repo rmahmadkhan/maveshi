c795b97aa1af04b0b24751c32570deba67e5af0c
Ahmad Khan <rmahmadkhan@icloud.com>
2022-04-25 00:28:42 +0500

    Created RegisterForm

---
+++ lib/ui/screens/register/components/register_form.dart
@@ -0,0 +1,53 @@
+import 'package:maveshi/all_utils.dart';
+
+class RegisterForm extends StatefulWidget {
+  const RegisterForm({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        MyTextField(controller: nameController, hintText: 'Name'),
+        const VerticalSpacing(),
+        MyTextField(
+          controller: emailController,
+          hintText: 'Email',
+          keyboardType: TextInputType.emailAddress,
+        ),
+        const VerticalSpacing(),
+        MyTextField(
+          controller: passwordController,
+          hintText: 'Password',
+          obscure: true,
+        ),
+        const VerticalSpacing(),
+        MyTextField(
+          controller: confirmPasswordController,
+          hintText: 'Confirm Password',
+          obscure: true,
+        ),
+        const VerticalSpacing(),
+        MyElevatedButton('Register', onTap: (_) {}),
+      ],
+    );
+  }
+}
