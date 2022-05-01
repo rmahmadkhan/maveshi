edcf89c47630a8c5048146549e2707be78047b27
Ahmad Khan <rmahmadkhan@icloud.com>
2022-05-02 01:27:00 +0500

    Created SetupFarmForm

---
+++ lib/ui/screens/setup_farm/components/setup_farm_form.dart
@@ -0,0 +1,34 @@
+import 'package:maveshi/all_utils.dart';
+import 'package:maveshi/ui/common_widgets/my_dropdown.dart';
+
+class SetupFarmForm extends StatefulWidget {
+  const SetupFarmForm({Key? key}) : super(key: key);
+
+  @override
+  State<SetupFarmForm> createState() => _SetupFarmFormState();
+}
+
+class _SetupFarmFormState extends State<SetupFarmForm> {
+  final nameController = TextEditingController();
+  final currencyController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        MyTextField(controller: nameController, hintText: 'Farm Name'),
+        const VerticalSpacing(),
+        MyDropdown(
+          controller: currencyController,
+          list: const ['USD', 'PKR', 'AED'],
+          hint: 'Select currency',
+        ),
+        const VerticalSpacing(),
+        MyElevatedButton(
+          'Finish',
+          onTap: (_) {},
+        ),
+      ],
+    );
+  }
+}
