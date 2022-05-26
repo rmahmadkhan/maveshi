ee8bd0bc6a0c2e94f4a6a3a9266fe6a9bc227048
Ahmad Khan <rmahmadkhan@icloud.com>
2022-05-26 21:58:44 +0500

    Create FarmDetails to select group

---
+++ lib/ui/screens/home/farm_details.dart
@@ -0,0 +1,76 @@
+import 'package:maveshi/all_utils.dart';
+import 'package:maveshi/models/enums/animal_type.dart';
+import 'package:maveshi/ui/common_widgets/my_dropdown.dart';
+import 'package:maveshi/ui/screens/home/animal_count_card.dart';
+
+class FarmDetails extends StatefulWidget {
+  const FarmDetails({Key? key}) : super(key: key);
+
+  @override
+  State<FarmDetails> createState() => _FarmDetailsState();
+}
+
+class _FarmDetailsState extends State<FarmDetails> {
+  final _groupController = TextEditingController();
+
+  @override
+  void dispose() {
+    _groupController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        MyDropdown(
+          controller: _groupController,
+          list: const ['All Groups', 'Group 1', 'Group 2'],
+          hint: 'All Groups',
+        ),
+        const VerticalSpacing(),
+        Row(
+          children: [
+            Expanded(
+              child: AnimalCountCard(
+                AnimalType.cow,
+                count: 10,
+                onTap: () {},
+              ),
+            ),
+            const HorizontalSpacing(),
+            Expanded(
+              child: AnimalCountCard(
+                AnimalType.buffalo,
+                count: 8,
+                onTap: () {},
+              ),
+            ),
+          ],
+        ),
+        const VerticalSpacing(of: 10),
+        Row(
+          children: [
+            Expanded(
+              child: AnimalCountCard(
+                AnimalType.goat,
+                count: 7,
+                onTap: () {},
+              ),
+            ),
+            const HorizontalSpacing(),
+            Expanded(
+              child: AnimalCountCard(
+                AnimalType.sheep,
+                count: 5,
+                onTap: () {},
+              ),
+            ),
+          ],
+        ),
+        const VerticalSpacing(),
+        MyElevatedButton('See All', onTap: (_) {}),
+      ],
+    );
+  }
+}
