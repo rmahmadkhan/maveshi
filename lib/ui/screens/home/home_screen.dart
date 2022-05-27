5d659a12fd9f04943d2c2a9e31042c6c6feefb2f
Ahmad Khan <rmahmadkhan@icloud.com>
2022-05-27 20:04:47 +0500

    Completed HomeScreen

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:maveshi/all_utils.dart';
+import 'package:maveshi/ui/common_widgets/my_card.dart';
+import 'package:maveshi/ui/screens/home/components/farm_details.dart';
+import 'package:maveshi/ui/screens/home/components/home_hawk_fab_menu.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName = '/HomeScreen';
@@ -8,10 +11,60 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BaseScaffold(
-      body: Column(
-        children: const [
-          MyText('Hi, Harry!'),
-        ],
+      body: HomeHawkFABMenu(
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: MyText(
+                  'Hi, Harry!',
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const VerticalSpacing(),
+              MyCard(
+                color: AppTheme.navyBlueColor,
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const MyText(
+                      'Harry\'s Cattle Farm',
+                      fontSize: 30,
+                      fontWeight: FontWeight.w500,
+                      color: AppTheme.whiteColor,
+                    ),
+                    const VerticalSpacing(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        MyText(
+                          'Total Active Animals:',
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                          color: AppTheme.whiteColor,
+                        ),
+                        MyText(
+                          '30',
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                          color: AppTheme.whiteColor,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const VerticalSpacing(),
+              const FarmDetails(),
+              const VerticalSpacing(),
+              Image.asset('assets/images/farm.png', fit: BoxFit.fitWidth),
+            ],
+          ),
+        ),
       ),
     );
   }
