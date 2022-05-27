3519c1aa89ee08f823a3bfd3fb30616b721337f8
Ahmad Khan <rmahmadkhan@icloud.com>
2022-05-27 20:04:20 +0500

    Create home_hawk_fab_menu.dart

---
+++ lib/ui/screens/home/components/home_hawk_fab_menu.dart
@@ -0,0 +1,44 @@
+import 'package:hawk_fab_menu/hawk_fab_menu.dart';
+import 'package:maveshi/all_utils.dart';
+
+class HomeHawkFABMenu extends StatelessWidget {
+  final Widget body;
+  const HomeHawkFABMenu({required this.body, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return HawkFabMenu(
+      openIcon: Icons.add,
+      closeIcon: Icons.close,
+      fabColor: AppTheme.orangeColor,
+      iconColor: AppTheme.whiteColor,
+      items: [
+        HawkFabMenuItem(
+          label: 'Add Animal',
+          ontap: () {
+            ScaffoldMessenger.of(context).hideCurrentSnackBar();
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Animal added!')),
+            );
+          },
+          icon: const Icon(Icons.pets),
+          color: AppTheme.camelColor,
+          labelColor: AppTheme.navyBlueColor,
+        ),
+        HawkFabMenuItem(
+          label: 'Add Group',
+          ontap: () {
+            ScaffoldMessenger.of(context).hideCurrentSnackBar();
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Group added!')),
+            );
+          },
+          icon: const Icon(Icons.add_business_outlined),
+          color: AppTheme.camelColor,
+          labelColor: AppTheme.navyBlueColor,
+        ),
+      ],
+      body: body,
+    );
+  }
+}
