318368a4b8fb1032d93781b420d1e42e8cae5674
Ahmad Khan <rmahmadkhan@icloud.com>
2022-05-29 01:09:12 +0500

    Create Animal model

---
+++ lib/models/animal.dart
@@ -0,0 +1,50 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:maveshi/models/enums/animal_obtained_by.dart';
+import 'package:maveshi/models/enums/gender.dart';
+
+part 'animal.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class Animal {
+  final String id;
+  final String tag;
+  final String name;
+  final String imagePath;
+  final Gender gender;
+  final String breed;
+  final DateTime dateOfBirth;
+  final int age;
+  final AnimalObtainedBy obtainedBy;
+  final DateTime farmJoiningDate;
+  final String gotFrom;
+  final double initialPrice;
+  final double currentPrice;
+  final String fatherId;
+  final String motherId;
+  final String groupId;
+  final String notes;
+
+  const Animal({
+    required this.id,
+    required this.tag,
+    required this.name,
+    required this.imagePath,
+    required this.gender,
+    required this.breed,
+    required this.dateOfBirth,
+    required this.age,
+    required this.obtainedBy,
+    required this.farmJoiningDate,
+    required this.gotFrom,
+    required this.initialPrice,
+    required this.currentPrice,
+    required this.fatherId,
+    required this.motherId,
+    required this.groupId,
+    required this.notes,
+  });
+
+  factory Animal.fromJson(Map<String, dynamic> json) => _$AnimalFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AnimalToJson(this);
+}
