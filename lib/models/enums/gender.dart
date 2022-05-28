2dcbc423bcf41ac5c91ea681785155eff2c54b77
Ahmad Khan <rmahmadkhan@icloud.com>
2022-05-29 01:08:35 +0500

    Create gender.dart

---
+++ lib/models/enums/gender.dart
@@ -0,0 +1,12 @@
+enum Gender { male, female }
+
+extension GenderExtension on Gender {
+  String get value {
+    switch (this) {
+      case Gender.male:
+        return 'Male';
+      case Gender.female:
+        return 'Female';
+    }
+  }
+}
