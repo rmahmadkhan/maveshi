05a81d5739603e819cf1e647daf10f6cb610e18e
Ahmad Khan <rmahmadkhan@icloud.com>
2022-05-30 18:07:13 +0500

    Implemented ResponsiveWrapper in main

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:maveshi/all_screens.dart';
 import 'package:maveshi/all_utils.dart';
 import 'package:maveshi/firebase_options.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,6 +17,14 @@ class MaveshiApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, child) => ResponsiveWrapper.builder(
+        child,
+        maxWidth: 480,
+        minWidth: 380,
+        defaultScale: true,
+        breakpoints: const [ResponsiveBreakpoint.autoScale(380)],
+        background: Container(color: const Color(0xFFF5F5F5)),
+      ),
       debugShowCheckedModeBanner: false,
       title: 'Maveshi',
       themeMode: ThemeMode.light,
