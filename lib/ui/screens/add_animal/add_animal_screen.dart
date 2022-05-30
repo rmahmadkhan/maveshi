65aaaa306f4ac0291b1e71043f83164e05bacc31
Ahmad Khan <rmahmadkhan@icloud.com>
2022-05-30 15:36:12 +0500

    Removed SafeArea from BaseScaffold & AddAnimalScreen

--- lib/ui/screens/add_animal/add_animal_screen.dart
+++ lib/ui/screens/add_animal/add_animal_screen.dart
@@ -65,190 +65,188 @@ class _AddAnimalScreenState extends State<AddAnimalScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: BaseScaffold(
-        appBar: AppBar(
-          backgroundColor: AppTheme.whiteColor,
-          foregroundColor: AppTheme.navyBlueColor,
-          title: const MyText(
-            'Add Animal',
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-          centerTitle: true,
-          elevation: 0,
+    return BaseScaffold(
+      appBar: AppBar(
+        backgroundColor: AppTheme.whiteColor,
+        foregroundColor: AppTheme.navyBlueColor,
+        title: const MyText(
+          'Add Animal',
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
         ),
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              ImageUploader(_imageUploaderCallback),
-              const VerticalSpacing(of: 10),
-              MyTextField(
-                controller: tagController,
-                caption: 'Tag*',
-                hintText: 'A-101',
-              ),
-              const VerticalSpacing(of: 10),
-              MyTextField(
-                controller: nameController,
-                caption: 'Name*',
-                hintText: 'Alpha',
-              ),
-              const VerticalSpacing(of: 10),
-              const MyText(
-                'Gender*',
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: AppTheme.navyBlueColor,
-              ),
-              const VerticalSpacing(of: 10),
-              Row(
-                children: [
-                  Expanded(
-                    child: MyRadioButtonTile(
-                      value: Gender.male,
-                      groupValue: gender,
-                      onChanged: onGenderChangeCallback,
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ImageUploader(_imageUploaderCallback),
+            const VerticalSpacing(of: 10),
+            MyTextField(
+              controller: tagController,
+              caption: 'Tag*',
+              hintText: 'A-101',
+            ),
+            const VerticalSpacing(of: 10),
+            MyTextField(
+              controller: nameController,
+              caption: 'Name*',
+              hintText: 'Alpha',
+            ),
+            const VerticalSpacing(of: 10),
+            const MyText(
+              'Gender*',
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: AppTheme.navyBlueColor,
+            ),
+            const VerticalSpacing(of: 10),
+            Row(
+              children: [
+                Expanded(
+                  child: MyRadioButtonTile(
+                    value: Gender.male,
+                    groupValue: gender,
+                    onChanged: onGenderChangeCallback,
+                  ),
+                ),
+                const HorizontalSpacing(),
+                Expanded(
+                  child: MyRadioButtonTile(
+                    value: Gender.female,
+                    groupValue: gender,
+                    onChanged: onGenderChangeCallback,
+                  ),
+                ),
+              ],
+            ),
+            const VerticalSpacing(of: 10),
+            MyTextField(
+              controller: breedController,
+              caption: 'Breed',
+              hintText: 'Australian',
+            ),
+            const VerticalSpacing(of: 10),
+            Row(
+              children: [
+                Expanded(
+                  child: DatePicker(
+                    onChanged: onDOBChangeCallback,
+                    caption: 'Date of Birth*',
+                  ),
+                ),
+                const HorizontalSpacing(),
+                Expanded(
+                  child: MyTextField(
+                    controller: ageController,
+                    caption: 'Age',
+                    hintText: '0',
+                    keyboardType: TextInputType.number,
+                    suffixWidget: const MyText(
+                      'years',
+                      color: AppTheme.lightNavyBlueColor,
+                      fontSize: 16,
                     ),
                   ),
-                  const HorizontalSpacing(),
-                  Expanded(
-                    child: MyRadioButtonTile(
-                      value: Gender.female,
-                      groupValue: gender,
-                      onChanged: onGenderChangeCallback,
+                ),
+              ],
+            ),
+            const VerticalSpacing(of: 10),
+            const MyText(
+              'Obtained By',
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: AppTheme.navyBlueColor,
+            ),
+            const VerticalSpacing(of: 10),
+            MyDropdown(
+              controller: obtainedByController,
+              list: [
+                AnimalObtainedBy.birth.value,
+                AnimalObtainedBy.purchase.value,
+                AnimalObtainedBy.gift.value,
+              ],
+              hint: 'Select',
+            ),
+            const VerticalSpacing(of: 10),
+            Row(
+              children: [
+                Expanded(
+                  child: DatePicker(
+                    caption: 'Joined farm on',
+                    onChanged: onJoiningDateChangeCallback,
+                  ),
+                ),
+                const HorizontalSpacing(),
+                Expanded(
+                  child: MyTextField(
+                    controller: gotFromController,
+                    caption: 'Got from',
+                    hintText: 'John',
+                  ),
+                ),
+              ],
+            ),
+            const VerticalSpacing(of: 10),
+            Row(
+              children: [
+                Expanded(
+                  child: MyTextField(
+                    controller: initialPriceController,
+                    caption: 'Initial Price',
+                    suffixWidget: const MyText(
+                      'PKR',
+                      color: AppTheme.lightNavyBlueColor,
+                      fontSize: 16,
                     ),
                   ),
-                ],
-              ),
-              const VerticalSpacing(of: 10),
-              MyTextField(
-                controller: breedController,
-                caption: 'Breed',
-                hintText: 'Australian',
-              ),
-              const VerticalSpacing(of: 10),
-              Row(
-                children: [
-                  Expanded(
-                    child: DatePicker(
-                      onChanged: onDOBChangeCallback,
-                      caption: 'Date of Birth*',
+                ),
+                const HorizontalSpacing(),
+                Expanded(
+                  child: MyTextField(
+                    controller: currentPriceController,
+                    caption: 'Current Price',
+                    suffixWidget: const MyText(
+                      'PKR',
+                      color: AppTheme.lightNavyBlueColor,
+                      fontSize: 16,
                     ),
                   ),
-                  const HorizontalSpacing(),
-                  Expanded(
-                    child: MyTextField(
-                      controller: ageController,
-                      caption: 'Age',
-                      hintText: '0',
-                      keyboardType: TextInputType.number,
-                      suffixWidget: const MyText(
-                        'years',
-                        color: AppTheme.lightNavyBlueColor,
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const VerticalSpacing(of: 10),
-              const MyText(
-                'Obtained By',
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: AppTheme.navyBlueColor,
-              ),
-              const VerticalSpacing(of: 10),
-              MyDropdown(
-                controller: obtainedByController,
-                list: [
-                  AnimalObtainedBy.birth.value,
-                  AnimalObtainedBy.purchase.value,
-                  AnimalObtainedBy.gift.value,
-                ],
-                hint: 'Select',
-              ),
-              const VerticalSpacing(of: 10),
-              Row(
-                children: [
-                  Expanded(
-                    child: DatePicker(
-                      caption: 'Joined farm on',
-                      onChanged: onJoiningDateChangeCallback,
-                    ),
-                  ),
-                  const HorizontalSpacing(),
-                  Expanded(
-                    child: MyTextField(
-                      controller: gotFromController,
-                      caption: 'Got from',
-                      hintText: 'John',
-                    ),
-                  ),
-                ],
-              ),
-              const VerticalSpacing(of: 10),
-              Row(
-                children: [
-                  Expanded(
-                    child: MyTextField(
-                      controller: initialPriceController,
-                      caption: 'Initial Price',
-                      suffixWidget: const MyText(
-                        'PKR',
-                        color: AppTheme.lightNavyBlueColor,
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                  const HorizontalSpacing(),
-                  Expanded(
-                    child: MyTextField(
-                      controller: currentPriceController,
-                      caption: 'Current Price',
-                      suffixWidget: const MyText(
-                        'PKR',
-                        color: AppTheme.lightNavyBlueColor,
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const VerticalSpacing(of: 10),
-              MyDropdown(
-                controller: fatherIdController,
-                list: const ['Aplha', 'Beta'],
-                hint: 'Select',
-                caption: 'Father',
-              ),
-              const VerticalSpacing(of: 10),
-              MyDropdown(
-                controller: motherIdController,
-                list: const ['Aplha', 'Beta'],
-                hint: 'Select',
-                caption: 'Mother',
-              ),
-              const VerticalSpacing(of: 10),
-              MyDropdown(
-                controller: groupIdController,
-                list: const ['Aplha', 'Beta'],
-                hint: 'Select',
-                caption: 'Group',
-              ),
-              const VerticalSpacing(of: 10),
-              MyTextField(
-                controller: notesController,
-                caption: 'Notes',
-                maxLines: 4,
-              ),
-              const VerticalSpacing(),
-              MyElevatedButton('Add Animal', onTap: (_) {}),
-            ],
-          ),
+                ),
+              ],
+            ),
+            const VerticalSpacing(of: 10),
+            MyDropdown(
+              controller: fatherIdController,
+              list: const ['Aplha', 'Beta'],
+              hint: 'Select',
+              caption: 'Father',
+            ),
+            const VerticalSpacing(of: 10),
+            MyDropdown(
+              controller: motherIdController,
+              list: const ['Aplha', 'Beta'],
+              hint: 'Select',
+              caption: 'Mother',
+            ),
+            const VerticalSpacing(of: 10),
+            MyDropdown(
+              controller: groupIdController,
+              list: const ['Aplha', 'Beta'],
+              hint: 'Select',
+              caption: 'Group',
+            ),
+            const VerticalSpacing(of: 10),
+            MyTextField(
+              controller: notesController,
+              caption: 'Notes',
+              maxLines: 4,
+            ),
+            const VerticalSpacing(),
+            MyElevatedButton('Add Animal', onTap: (_) {}),
+          ],
         ),
       ),
     );
