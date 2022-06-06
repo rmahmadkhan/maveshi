a7ec893e12049b69233be112e12e348904d42552
Ahmad Khan <rmahmadkhan@icloud.com>
2022-06-06 16:01:14 +0500

    Create animal_events_body.dart

---
+++ lib/ui/screens/animal_details/components/animal_events_body.dart
@@ -0,0 +1,65 @@
+import 'package:intl/intl.dart';
+import 'package:maveshi/all_utils.dart';
+import 'package:maveshi/models/animal_event.dart';
+import 'package:maveshi/models/enums/animal_event_type.dart';
+import 'package:maveshi/ui/common_widgets/my_card.dart';
+
+class AnimalEventsBody extends StatelessWidget {
+  final List<AnimalEvent>? events;
+
+  const AnimalEventsBody(this.events, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (events == null || events!.isEmpty) return const SizedBox.shrink();
+
+    return Column(
+      children: [
+        const Divider(
+          thickness: 1,
+          color: AppTheme.navyBlueColor,
+          height: 30,
+        ),
+        MyCard(
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              children: [
+                const MyText(
+                  'Latest Events',
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+                const VerticalSpacing(of: 10),
+                ListView.separated(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) => Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      MyText(
+                        events![index].event.value,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: AppTheme.navyBlueColor,
+                      ),
+                      MyText(
+                        DateFormat.yMMMd().format(events![index].dateTime),
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: AppTheme.navyBlueColor,
+                      ),
+                    ],
+                  ),
+                  separatorBuilder: (context, index) =>
+                      const Divider(thickness: 1, height: 20),
+                  itemCount: events!.length,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
