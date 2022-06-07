c18a13f21e64da6ec171444b4d0949ac34d680b5
Ahmad Khan <rmahmadkhan@icloud.com>
2022-06-07 20:18:16 +0500

    Create GroupViewScreen

---
+++ lib/ui/screens/group_view/group_view_screen.dart
@@ -0,0 +1,12 @@
+import 'package:maveshi/all_utils.dart';
+
+class GroupViewScreen extends StatelessWidget {
+  static const routeName = '/GroupViewScreen';
+
+  const GroupViewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
