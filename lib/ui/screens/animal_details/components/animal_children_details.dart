e314ea53134f76bbc20342054162a5cad464f1bd
Ahmad Khan <rmahmadkhan@icloud.com>
2022-06-07 17:31:15 +0500

    Create animal_children_details.dart

---
+++ lib/ui/screens/animal_details/components/animal_children_details.dart
@@ -0,0 +1,43 @@
+import 'package:maveshi/all_utils.dart';
+import 'package:maveshi/models/animal.dart';
+import 'package:maveshi/ui/screens/animal_details/components/animal_card.dart';
+
+class AnimalChildrenDetails extends StatelessWidget {
+  final List<Animal>? animals;
+
+  const AnimalChildrenDetails(this.animals, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (animals == null || animals!.isEmpty) return const SizedBox.shrink();
+
+    return Column(
+      children: [
+        const Divider(
+          thickness: 1,
+          color: AppTheme.navyBlueColor,
+          height: 30,
+        ),
+        Column(
+          children: [
+            const MyText(
+              'Children',
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+            ),
+            const VerticalSpacing(of: 10),
+            ListView.separated(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemBuilder: (context, index) =>
+                  AnimalCard(animals![index], height: 50),
+              separatorBuilder: (context, index) =>
+                  const VerticalSpacing(of: 10),
+              itemCount: animals!.length,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
