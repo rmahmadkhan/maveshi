acbee2cead9f720e8866c16295d3059fce6f4c94
Ahmad Khan <rmahmadkhan@icloud.com>
2022-06-12 03:34:30 +0500

    Implemented FarmDetails

--- lib/ui/screens/home/components/farm_details.dart
+++ lib/ui/screens/home/components/farm_details.dart
@@ -1,80 +1,77 @@
 import 'package:maveshi/all_screens.dart';
 import 'package:maveshi/all_utils.dart';
-import 'package:maveshi/global.dart';
 import 'package:maveshi/ui/screens/home/components/animal_count_card.dart';
 
-class FarmDetails extends StatefulWidget {
+class FarmDetails extends StatelessWidget {
   const FarmDetails({Key? key}) : super(key: key);
 
   @override
-  State<FarmDetails> createState() => _FarmDetailsState();
-}
-
-class _FarmDetailsState extends State<FarmDetails> {
-  final _groupController = TextEditingController();
-
-  @override
-  void dispose() {
-    _groupController.dispose();
-    super.dispose();
+  Widget build(BuildContext context) {
+    return Consumer<FarmProvider>(
+      builder: (context, provider, child) => Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: AnimalCountCard(
+                  AnimalType.cow,
+                  count: provider.cows.length,
+                  onTap: () =>
+                      _onTapAnimalCard(context, provider.cows, AnimalType.cow),
+                ),
+              ),
+              const HorizontalSpacing(),
+              Expanded(
+                child: AnimalCountCard(
+                  AnimalType.buffalo,
+                  count: provider.buffaloes.length,
+                  onTap: () => _onTapAnimalCard(
+                      context, provider.buffaloes, AnimalType.buffalo),
+                ),
+              ),
+            ],
+          ),
+          const VerticalSpacing(of: 10),
+          Row(
+            children: [
+              Expanded(
+                child: AnimalCountCard(
+                  AnimalType.goat,
+                  count: provider.goats.length,
+                  onTap: () => _onTapAnimalCard(
+                      context, provider.goats, AnimalType.goat),
+                ),
+              ),
+              const HorizontalSpacing(),
+              Expanded(
+                child: AnimalCountCard(
+                  AnimalType.sheep,
+                  count: provider.sheeps.length,
+                  onTap: () => _onTapAnimalCard(
+                      context, provider.sheeps, AnimalType.sheep),
+                ),
+              ),
+            ],
+          ),
+          const VerticalSpacing(),
+          MyElevatedButton(
+            'See All',
+            onTap: (_) =>
+                _onTapAnimalCard(context, provider.farm?.animals ?? [], null),
+          ),
+        ],
+      ),
+    );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        MyDropdown(
-          controller: _groupController,
-          list: const ['All Groups', 'Group 1', 'Group 2'],
-          hint: 'All Groups',
-        ),
-        const VerticalSpacing(),
-        Row(
-          children: [
-            Expanded(
-              child: AnimalCountCard(
-                AnimalType.cow,
-                count: 10,
-                onTap: () {},
-              ),
-            ),
-            const HorizontalSpacing(),
-            Expanded(
-              child: AnimalCountCard(
-                AnimalType.buffalo,
-                count: 8,
-                onTap: () {},
-              ),
-            ),
-          ],
-        ),
-        const VerticalSpacing(of: 10),
-        Row(
-          children: [
-            Expanded(
-              child: AnimalCountCard(
-                AnimalType.goat,
-                count: 7,
-                onTap: () {},
-              ),
-            ),
-            const HorizontalSpacing(),
-            Expanded(
-              child: AnimalCountCard(
-                AnimalType.sheep,
-                count: 5,
-                onTap: () {},
-              ),
-            ),
-          ],
-        ),
-        const VerticalSpacing(),
-        MyElevatedButton(
-          'See All',
-          onTap: (_) => Navigator.pushNamed(context, GroupViewScreen.routeName,
-              arguments: animals),
-        ),
-      ],
-    );
+  void _onTapAnimalCard(
+      BuildContext context, List<Animal> animals, AnimalType? type) {
+    if (animals.isEmpty) {
+      EasyLoading.showInfo(
+          'No ${type?.value.toLowerCase() ?? 'animal'}s in the farm!');
+    } else {
+      Navigator.pushNamed(context, GroupViewScreen.routeName,
+          arguments: animals);
+    }
   }
 }
